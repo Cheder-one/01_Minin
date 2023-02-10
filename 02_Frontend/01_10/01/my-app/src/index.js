58fb1f99a43e0c2a3cc5748c67b38610ccc8e94f
Cheder-one <cherbadenis1@gmail.com>
2023-02-10 03:41:41 +0300

    u

--- 02_Frontend/01_10/01/my-app/src/index.js
+++ 02_Frontend/01_10/01/my-app/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import reactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const elem = <h1>Hello</h1>
 
-console.log(elem);
+reactDom.render(elem, document.getElementById('root'))
+
