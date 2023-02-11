ed34fedb80d8f1e442fa946df11aa9036b0baae9
Cheder-one <cherbadenis1@gmail.com>
2023-02-11 18:55:12 +0300

    u

--- 02_Frontend/01_10/01/my-app/src/components/counter.jsx
+++ 02_Frontend/01_10/01/my-app/src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Counter = () => {
    const [count, setCount] = useState(0);
+   const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
 
    const formatCount = () => {
       return count === 0 ? "empty" : count;
@@ -14,17 +15,40 @@ const Counter = () => {
    };
 
    const handleIncrement = () => {
-      setCount(count + 1);
+      setCount((prevState) => prevState + 1);
+   };
+   const handleDecrement = () => {
+      setCount((prevState) => prevState - 1);
+   };
+   const handleTagChange = () => {
+      setTags(["tag4", "tag5"]);
    };
 
    return (
       <>
+         <ul>
+            {tags.map((tag) => (
+               <li
+                  key={tag}
+                  className="btn btn-primary btm-sm m-2"
+                  onClick={handleTagChange}
+               >
+                  {tag}
+               </li>
+            ))}
+         </ul>
          <span className={getBadgeClasses()}>{formatCount()}</span>
          <button
             className="btn btn-primary btn-sm m-2"
             onClick={handleIncrement}
          >
             +
+         </button>
+         <button
+            className="btn btn-primary btn-sm m-2"
+            onClick={handleDecrement}
+         >
+            -
          </button>
       </>
    );
--- 02_Frontend/01_10/01/my-app/src/index.js
+++ 02_Frontend/01_10/01/my-app/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
-import reactDom from "react-dom";
-import "bootstrap/dist/css/bootstrap.css"
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./components/counter";
 
 const App = () => {
-   return (
-      <Counter />
-   )
+   return <Counter />;
 };
 
-reactDom.render(<App />, document.getElementById("root"));
-
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<App />);
