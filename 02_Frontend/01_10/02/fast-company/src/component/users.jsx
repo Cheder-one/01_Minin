0a9498997456966dd7778f339d3cc353cf679aa8
Cheder-one <cherbadenis1@gmail.com>
2023-02-18 14:43:21 +0000

    u

--- 02_Frontend/01_10/02/fast-company/src/component/users.jsx
+++ 02_Frontend/01_10/02/fast-company/src/component/users.jsx
@@ -1,36 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import API from "../api/index.api";
 
-
-const Users = () => {
-   const [usersArray, setUsersArray] = useState(API.users.fetchAll())
-
-   console.log(API.users.fetchAll()[0]);
-
+const renderUsers = (usersArray) => {
    return (
-      <>
-         <table className="table">
-            <thead>
-               <tr>
-                  <th scope="col">Имя</th>
-                  <th scope="col">Качества</th>
-                  <th scope="col">Профессия</th>
-                  <th scope="col">Кол-во встреч</th>
-                  <th scope="col">Рейтинг</th>
-               </tr>
-            </thead>
+      <table className="table">
+         <thead>
+            <tr>
+               <th scope="col">Имя</th>
+               <th scope="col">Качества</th>
+               <th scope="col">Профессия</th>
+               <th scope="col">Кол-во встреч</th>
+               <th scope="col">Рейтинг</th>
+            </tr>
+         </thead>
+
+         {usersArray &&
+
             <tbody>
-               <tr>
-                  {/* {usersArray.map((item) => (
-                     <td key={_id}>
-                        {item}
-                     </td>
-                  ))} */}
-               </tr>
-            </tbody>
-         </table>
-      </>
+
+               {usersArray.map(el => (
+
+                  <tr key={el._id}>
+
+                     <td >{el.name}</td>
+
+                  </tr>
+
+               ))}
+
+            </tbody>}
+
+      </table>
+
    )
 }
 
+const Users = () => {
+
+   const [usersArray, setUsersArray] = useState(null)
+
+   useEffect(() => {
+
+      const fetchData = async () => {
+
+         const data = await API.users.fetchAll();
+
+         setUsersArray(data);
+
+      }; fetchData();
+   }, []); console.log(API.users.fetchAll()[0]._id); return (<>           {renderUsers(usersArray)}        </>)
+}
 export default Users
