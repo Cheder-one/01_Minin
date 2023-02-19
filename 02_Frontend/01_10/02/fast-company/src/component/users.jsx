adb4f3c0e877c77f17ae4e36e9771fd098b71b35
Cheder-one <cherbadenis1@gmail.com>
2023-02-19 15:35:09 +0300

    u

--- 02_Frontend/01_10/02/fast-company/src/component/users.jsx
+++ 02_Frontend/01_10/02/fast-company/src/component/users.jsx
@@ -1,80 +1,78 @@
 import React, { useState } from "react";
 import API from "../api/index.api";
 
-// TODO Разбить модули по отдельным функциям
-
 const Users = () => {
-   const [users, setUsersArray] = useState(API.users.fetchAll());
+  const [users, setUsersArray] = useState(API.users.fetchAll());
+  const numberOfUsers = users.length;
 
-   const handleDeleteUser = (id) => {
-      setUsersArray(users.filter((user) => user._id !== id));
-   };
+  const handleDeleteUser = (id) => {
+    setUsersArray(users.filter((user) => user._id !== id));
+  };
 
-   const calcNumberUsers = () => {
-      return users.length > 0
-         ? `${users.length} Человек готовы встретиться с тобой`
-         : "Список встреч пуст";
-   };
+  const calcNumberUsers = () => {
+    return numberOfUsers
+      ? `${numberOfUsers} Человек готовы встретиться с тобой`
+      : "Список встреч пуст";
+  };
 
-   return (
-      <>
-         <h5>
-            <span
-               className={`badge bg-${users.length > 0 ? "primary" : "danger"}`}
-            >
-               {calcNumberUsers()}
-            </span>
-         </h5>
+  const renderBadge = () => {
+    const badgeClass = numberOfUsers > 0 ? "primary" : "danger";
+    const badgeText = calcNumberUsers();
+    return (
+      <h5>
+        <span className={`badge bg-${badgeClass}`}>{badgeText}</span>
+      </h5>
+    );
+  };
 
-         <table className="table">
-            <thead>
-               <tr>
-                  <th scope="col">Имя</th>
-                  <th scope="col">Качества</th>
-                  <th scope="col">Профессия</th>
-                  <th scope="col">Кол-во встреч</th>
-                  <th scope="col">Рейтинг</th>
-                  <th scope="col"></th>
-               </tr>
-            </thead>
-            <tbody>
-               {users.map((user) => {
-                  return (
-                     <tr key={user._id}>
-                        <td>{user.name}</td>
-                        <td>
-                           {user.qualities.map((el) => {
-                              return (
-                                 <span
-                                    className={`badge bg-${el.color}`}
-                                    key={el._id}
-                                 >
-                                    {el.name}
-                                 </span>
-                              );
-                           })}
-                        </td>
+  const renderUsersTable = () => {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Кол-во встреч</th>
+            <th scope="col">Рейтинг</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td>{user.name}</td>
+              <td>
+                {user.qualities.map((el) => (
+                  <span className={`badge bg-${el.color}`} key={el._id}>
+                    {el.name}
+                  </span>
+                ))}
+              </td>
+              <td key={user.profession._id}>{user.profession.name}</td>
+              <td>{user.completedMeetings}</td>
+              <td>{user.rate}/5</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDeleteUser(user._id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
 
-                        <td key={user.profession._id}>
-                           {user.profession.name}
-                        </td>
-                        <td>{user.completedMeetings}</td>
-                        <td>{user.rate}/5</td>
-                        <td>
-                           <button
-                              className="btn btn-danger"
-                              onClick={() => handleDeleteUser(user._id)}
-                           >
-                              Delete
-                           </button>
-                        </td>
-                     </tr>
-                  );
-               })}
-            </tbody>
-         </table>
-      </>
-   );
+  return (
+    <>
+      {renderBadge()}
+      {renderUsersTable()}
+    </>
+  );
 };
 
 export default Users;
