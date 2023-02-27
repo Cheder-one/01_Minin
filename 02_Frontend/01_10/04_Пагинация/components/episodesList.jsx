2ef80e71f0161083a701eb5a50d5ce0a06cfdd1e
Cheder-one <cherbadenis1@gmail.com>
2023-02-27 11:22:05 +0300

    u

--- 02_Frontend/01_10/04_Пагинация/components/episodesList.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EpisodesList = () => {
-  return <div>EpisodesList</div>;
-};
-
-export default EpisodesList;
--- 02_Frontend/01_10/04_Пагинация/src/App.js
+++
@@ -1,9 +0,0 @@
-import EpisodesList from "../components/episodesList";
-
-function App() {
-  return (
-    <div className="App">
-      <EpisodesList />
-    </div>
-  )
-}
