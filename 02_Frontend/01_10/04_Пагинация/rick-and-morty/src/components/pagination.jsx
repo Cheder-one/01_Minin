c2dccdccc16ab12bc7606a5dc9ab6a2edb58c47e
Cheder-one <cherbadenis1@gmail.com>
2023-02-27 18:44:23 +0300

    u

--- 02_Frontend/01_10/04_Пагинация/rick-and-morty/src/components/pagination.jsx
+++ 02_Frontend/01_10/04_Пагинация/rick-and-morty/src/components/pagination.jsx
@@ -3,15 +3,18 @@ import _ from "lodash";
 
 const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange } = props;
+  const pageCount = Math.ceil(itemsCount / pageSize);
+
+  const pages = _.range(1, pageCount + 1);
 
   return (
     <nav>
       <ul className="pagination">
-        <li className="page-item">
-          <a href="#" className="page-link">
-            1
-          </a>
-        </li>
+        {pages.map((page) => (
+          <li className="page-item">
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
