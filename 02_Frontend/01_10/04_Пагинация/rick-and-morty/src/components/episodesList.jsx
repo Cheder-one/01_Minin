60625169b122da29373f92eb17741774e253dc10
Cheder-one <cherbadenis1@gmail.com>
2023-03-05 00:40:46 +0300

    u

--- 02_Frontend/01_10/04_Пагинация/rick-and-morty/src/components/episodesList.jsx
+++ 02_Frontend/01_10/04_Пагинация/rick-and-morty/src/components/episodesList.jsx
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
-import { episodes } from "../fakeApi/episodeApi";
+import React, { useEffect, useState } from "react";
+import { fetchAll, fetchYears } from "../fakeApi/episodeApi";
 import { paginate } from "../utils/paginate";
 import Episode from "./episode";
 import GroupList from "./groupList";
 import Pagination from "./pagination";
 
 const EpisodesList = () => {
-  // const [episodes] = useState([]);
+  const [episodes, setEpisodes] = useState([]);
+  const [years, setYears] = useState([]);
+  const [filter, setFilter] = useState();
 
   const [currentPage, setCurrentPage] = useState(1);
-
   const count = episodes.length;
   const pageSize = 8;
+
+  const getEpisodes = () => {
+    fetchAll(years).then((response) => {
+      setEpisodes(response);
+      setCurrentPage(1);
+    });
+  };
+
+  useEffect(() => {
+    fetchYears().then((response) => setYears(response));
+  });
+
+  const handleFilterChange = (filter) => {
+    setFilter(filter);
+  };
+
   const handlePageChange = (pageIndex) => {
     setCurrentPage(pageIndex);
   };
@@ -22,7 +39,11 @@ const EpisodesList = () => {
     <div className="container pt-2">
       <div className="row">
         <div className="col-4">
-          <GroupList />
+          <GroupList
+            items={years}
+            filter={filter}
+            onChangeFilter={handleFilterChange}
+          />
         </div>
         <div className="col-8">
           <div className="row">
