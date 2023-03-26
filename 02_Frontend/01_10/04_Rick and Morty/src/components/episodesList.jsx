166ca796c50700a7dc405d8480670f8d7da2d786
Cheder-one <cherbadenis1@gmail.com>
2023-03-26 10:57:08 +0300

    u

--- 02_Frontend/01_10/04_Rick and Morty/src/components/episodesList.jsx
+++ 02_Frontend/01_10/04_Rick and Morty/src/components/episodesList.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { episodes } from "../fakeStorage/episodes";
 
 const EpisodesList = () => {
   return (
     <div className="container">
       <div className="row"></div>
+      {episodes.map((episode) => (
+        <div key={episode.id} style={{ height: "200px" }} className="col-3">
+          {episode.name}
+        </div>
+      ))}
     </div>
   );
 };
